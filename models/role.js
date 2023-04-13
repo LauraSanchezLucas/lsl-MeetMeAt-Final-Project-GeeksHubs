463c456a2bb4d8e3383cadd31265f9da5c8765d6
Laura <Lausnclu@hotmail.com>
2023-04-13 13:00:09 +0200

    feat: Relation in the role and user model

--- models/role.js
+++ models/role.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association 
+      Role.hasMany(models.User,{
+        foreignKey: 'role_id'
+      });
     }
   }
   Role.init({
--- models/user.js
+++ models/user.js
@@ -11,6 +11,20 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(
+        models.Role,{
+          foreignKey: 'role_id'
+        });
+      User.hasMany(
+        models.Appointment,{
+        foreignKey: 'user_id'
+      });
+      User.belongsToMany(
+        models.Business,{
+          foreignKey: 'user_id'
+        }
+      )
+
     }
   }
   User.init({
