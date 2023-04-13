755a54a4ed971ec7aa2fcce8d3245fe874bdfd28
Laura <Lausnclu@hotmail.com>
2023-04-13 13:16:53 +0200

    feat: Relation appointment  and event model with foreing key

--- models/appointment.js
+++ models/appointment.js
@@ -4,13 +4,20 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Appointment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+
+      Appointment.belongsTo(models.User, {
+        foreignKey: 'user_id'
+      });
+
+      Appointment.belongsTo(models.Event,{
+        foreignKey: 'event_id'
+      });
+
+      Appointment.belongsTo(models.Business,{
+        foreignKey: 'business_id'
+      });
     }
   }
   Appointment.init({
--- models/user.js
+++ models/user.js
@@ -4,27 +4,20 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
-      User.belongsTo(
-        models.Role,{
-          foreignKey: 'role_id'
-        });
-      User.hasMany(
-        models.Appointment,{
+
+      User.belongsTo(models.Role,{
+        foreignKey: 'role_id'
+      });
+
+      User.hasMany(models.Appointment,{
         foreignKey: 'user_id'
       });
-      User.belongsToMany(
-        models.Business,{
-          foreignKey: 'user_id'
-        }
-      )
 
+      User.belongsToMany(models.Business,{
+        foreignKey: 'user_id'
+      });
     }
   }
   User.init({
