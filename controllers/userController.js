d3cd04e57d3bfcba274b295bdc41d7875423fb52
Laura <Lausnclu@hotmail.com>
2023-04-14 20:43:23 +0200

    feat: Update profile created and routed, all working rigth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const bcrypt = require('bcrypt');
 
 
 const userController = {};
@@ -18,13 +19,61 @@ userController.profile = async(req, res) => {
     } catch (error) {
         return res.status(500).json(
             {
-                success: true,
+                success: false,
                 message: 'Something went wrong',
                 error_message: error.message
             }
         )
-        
     };
 }
+
+userController.updateProfile = async(req, res) => {
+    try {
+        const { name, surname, email, phone, password } = req.body;
+        const userId = req.userId;
+
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+
+        const updateProfile = await User.update(
+            {
+                name: name,
+                surname: surname,
+                email: email,
+                phone: phone,
+                password: encryptedPassword
+            },
+            {
+                where:{
+                    id: userId
+                }
+            }
+        );
+
+        if(!updateProfile){
+            return res.send(
+                {
+                success: false,
+                message: 'Can´t update user profile',
+                error_message: error.message
+                }
+            )
+        }
+        return res.send(
+            {
+            success: true,
+            message: 'Update user profile successfully',
+            updateProfile: updateProfile
+            }
+        )
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Something went wrong',
+                error_message: error.message
+            }
+        )
+    }
+};
 
 module.exports = userController;
--- views/userRouter.js
+++ views/userRouter.js
@@ -4,7 +4,8 @@ const verifyToken= require('../middleware/verifyToken')
 const router = require ('express').Router();
 
 // USER
-router.get('/profile', verifyToken, userController.profile)
+router.get('/profile', verifyToken, userController.profile);
+router.put('/updateprofile', verifyToken, userController.updateProfile)
 
 
 
