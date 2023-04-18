a76ba3993295a0406e6b91eadfd0c15c115943ab
Laura <Lausnclu@hotmail.com>
2023-04-18 11:47:08 +0200

    feat: clear code and checked endpoints userController and authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,91 +1,86 @@
-const { User } = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { User } = require("../models");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const authController = {};
 
 // LOGIN
 authController.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne(
-            {
+try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
         where: {
             email: email,
         },
-        });
-        if (!user) {
-        return res.send('Unfortunately we do not recognise those details');
-        }
-        const isMatch = bcrypt.compareSync(password, user.password);
-        if (!isMatch) {
-        return res.send('Unfortunately we do not recognise those details');
-        }
-        const token = jwt.sign(
-        {
-            userId: user.id,
-            email: user.email,
-            roleId: user.role_id,
-            name: user.name
-        },
-        'secreto',
-        { expiresIn: '2h' }
-        );
-        return res.json(
-            {
+    });
+    if (!user) {
+        return res.json({
             success: true,
-            message: 'Login successfully',
-            token: token
-            }
-        );
-    } catch (error) {
-        return res.status(500).json(
-            {
-            success: false,
-            message: 'Somenthing went wrong',
-            error: error.message
-        }
-    )
-    }
+            message: 'Unfortunately we do not recognise those details',
+        })
+    };
+    const isMatch = bcrypt.compareSync(password, user.password);
+    if (!isMatch) {
+        return res.json({
+            success: true,
+            message: 'Unfortunately we do not recognise those details',
+        })
+    };
+
+    const token = jwt.sign(
+    {
+        userId: user.id,
+        email: user.email,
+        roleId: user.role_id,
+        name: user.name,
+    },
+    "secreto",
+    { expiresIn: "2h" }
+    );
+    return res.json({
+        success: true,
+        message: "Login successfully",
+        token: token,
+    });
+} catch (error) {
+    return res.status(500).json({
+        success: false,
+        message: "Somenthing went wrong",
+        error: error.message,
+    });
+}
 };
 
-// LOGIN
+// REGISTER
+authController.register = async (req, res) => {
+try {
+    const { name, surname, email, phone, password } = req.body;
 
-authController.register = async(req, res) => {
-    try {
-        const { name, surname, email, phone, password } = req.body;
+    const encryptedPassword = bcrypt.hashSync(password, 10);
 
-        const encryptedPassword = bcrypt.hashSync(password, 10);
+    const newUser = {
+        name: name,
+        surname: surname,
+        email: email,
+        phone: phone,
+        password: encryptedPassword,
+        role_id: 3,
+    };
 
-        const newUser = {
-            name: name,
-            surname: surname,
-            email: email,
-            phone: phone,
-            password: encryptedPassword,
-            role_id: 3
-        }
-
-        const user = await User.create(newUser);
-        return res.json(
-            {
-                success: true,
-                message: 'Registered user successfully',
-                user: user
-            }
-        )
-    } catch (error) {
-        return res.status(500).json(
-            {
-                success: true,
-                message: 'Something went wrong',
-                error_message: error.message
-            }
-        )
-    }
+    const user = await User.create(newUser);
+    return res.json({
+        success: true,
+        message: "Registered user successfully",
+        user: user,
+    });
+} catch (error) {
+    return res.status(500).json({
+        success: true,
+        message: "Something went wrong",
+        error_message: error.message,
+    });
+}
 };
-
-
-
 
 module.exports = authController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,78 +1,66 @@
-const { User } = require('../models');
-const bcrypt = require('bcrypt');
-
+const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 const userController = {};
 
-userController.profile = async(req, res) => {
-    try {
+// PROFILE
+userController.profile = async (req, res) => {
+try {
+    const user = await User.findByPk(req.userId);
 
-        const user = await User.findByPk(req.userId);
-
-        return res.json(
-            {
-                success: true,
-                message: 'access profile successfully',
-                user: user
-            }
-        );
-    } catch (error) {
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Something went wrong',
-                error_message: error.message
-            }
-        )
-    };
+    return res.json({
+        success: true,
+        message: 'Access profile successfully',
+        user: user,
+    });
+} catch (error) {
+    return res.status(500).json({
+        success: false,
+        message: 'Something went wrong',
+        error_message: error.message,
+    });
 }
+};
 
-userController.updateProfile = async(req, res) => {
-    try {
-        const { name, surname, email, phone, password } = req.body;
-        const userId = req.userId;
-        const encryptedPassword = bcrypt.hashSync(password, 10);
+// UPDATE MY OWN PROFILE
+userController.updateProfile = async (req, res) => {
+try {
+    const { name, surname, email, phone, password } = req.body;
+    const userId = req.userId;
+    const encryptedPassword = bcrypt.hashSync(password, 10);
 
-        const updateProfile = await User.update(
-            {
-                name: name,
-                surname: surname,
-                email: email,
-                phone: phone,
-                password: encryptedPassword
-            },
-            {
-                where:{
-                    id: userId
-                }
-            }
-        );
-
-        if(!updateProfile){
-            return res.send(
-                {
-                success: false,
-                message: 'Can´t update user profile',
-                error_message: error.message
-                }
-            )
-        }
-        return res.send(
-            {
-            success: true,
-            message: 'Update user profile successfully',
-            updateProfile: updateProfile
-            }
-        )
-    } catch (error) {
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Something went wrong',
-                error_message: error.message
-            }
-        )
+    const updateProfile = await User.update(
+    {
+        name: name,
+        surname: surname,
+        email: email,
+        phone: phone,
+        password: encryptedPassword,
+    },
+    {
+        where: {
+            id: userId,
+        },
     }
+    );
+    if (!updateProfile) {
+    return res.send({
+        success: false,
+        message: 'Can not update user profile',
+        error_message: error.message,
+    })};
+    return res.send({
+        success: true,
+        message: 'Update user profile successfully',
+        updateProfile: updateProfile,
+    });
+} catch (error) {
+    return res.status(500).json({
+        success: false,
+        message: 'Something went wrong',
+        error_message: error.message,
+    })
+}
 };
 
 module.exports = userController;
